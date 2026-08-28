17fd3187ef3979e7f0409cdded4d385da9b716ce
Rupayan <rikdasrik297@gmail.com>
2026-08-28 20:00:49 +0530

    Implement TokenBucket algorithm and access control middleware for rate limiting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { accessBucket } from './middlewares/rateLimiter.middleware.js';
+import { TokenBucket } from './rate_limiter/algorithms/tokenBucket.algo.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const tokenBucket = new TokenBucket(3, 1); // capacity of 3 tokens, refill rate of 1 token per second
+
+
+app.get(
+    '/api/:userId',
+    accessBucket(tokenBucket),
+    (req, res) => {
+        res.status(200).send('Request allowed');
+    }
+);
+
 
 app.get('/healthcheck', (req, res) => {
   res.status(200).send('OK');
