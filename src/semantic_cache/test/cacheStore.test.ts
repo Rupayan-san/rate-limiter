e2adce9ce41c2e9696b0b8f30b3b9f73a97bd137
Rupayan <rikdasrik297@gmail.com>
2026-09-10 13:51:40 +0530

    tested cache-storing

--- src/semantic_cache/test/cacheStore.test.ts
+++ src/semantic_cache/test/cacheStore.test.ts
@@ -1,18 +1,20 @@
 import { createCacheService, getCacheService } from "../services/semantic_cache.service.js";
 import { describe, it, expect } from "vitest";
-import { client } from "../../config/redis.config.js";
-import { createClient } from "redis";
-
 
 
 describe("Cache Store Tests", () => {
     it("should create a cache entry in Redis", async () => {
         await createCacheService();
-    });
+    }, 10000); // Increase timeout for async operations
 
-    // it("should retrieve a cache entry from Redis", async () => {
-    //     const cache_id = "123";
-    //     const cacheEntry = await getCacheService(cache_id);
-    //     expect(cacheEntry).toBeDefined();
-    // });
+    it("should retrieve a cache entry from Redis", async () => {
+        const cacheId = "123";
+
+        const cacheEntry = await getCacheService(cacheId);
+
+        expect(cacheEntry).toBeDefined();
+        expect(cacheEntry?.query).toBe("How do I reset my password?");
+        expect(cacheEntry?.response).toBe("Go to Settings...");
+        expect(cacheEntry?.embedding).toBeDefined();
+    });
 });
